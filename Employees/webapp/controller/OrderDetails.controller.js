9857881f211ec08895c7f65c629f9dea49b25497
Logali-Group <training@logaligroupo.com>
2020-12-28 13:33:34 +0000

    19 Factory

--- Employees/webapp/controller/OrderDetails.controller.js
+++ Employees/webapp/controller/OrderDetails.controller.js
@@ -31,10 +31,36 @@ sap.ui.define([
             }
         },
 
-        onClearSignature: function(oEvent) {
+        onClearSignature: function (oEvent) {
             var signature = this.byId("signature");
             signature.clear();
-        }
+        },
 
+        factoryOrderDetails : function(listId, oContext) {
+
+            var contextObject = oContext.getObject();
+            contextObject.Currency = "EUR";
+            var unitsInStock = oContext.getModel().getProperty("/Products(" + contextObject.ProductID + ")/UnitsInStock");
+
+            if (contextObject.Quantity <= unitsInStock) {
+                var objectListItem = new sap.m.ObjectListItem({
+                    title : "{odataNorthwind>/Products(" + contextObject.ProductID + ")/ProductName} ({odataNorthwind>Quantity})",
+                    number : "{parts: [ {path: 'odataNorthwind>UnitPrice'}, {path: 'odataNorthwind>Currency'}], type:'sap.ui.model.type.Currency', formatOptions: {showMeasure: false}}",
+                    numberUnit: "{odataNorthwind>Currency}"
+                });
+                return objectListItem;
+            } else {
+                var customListItem = new sap.m.CustomListItem({
+                    content: [
+                        new sap.m.Bar({
+                            contentLeft: new sap.m.Label({ text: "{odataNorthwind>/Products(" + contextObject.ProductID + ")/ProductName} ({odataNorthwind>Quantity})"}),
+                            contentMiddle:new sap.m.ObjectStatus({ text: "{i18n>availableStock} {odataNorthwind>/Products(" + contextObject.ProductID + ")/UnitsInStock}", state: "Error"}),
+                            contentRight: new sap.m.Label({text:"{parts: [ {path: 'odataNorthwind>UnitPrice'}, {path: 'odataNorthwind>Currency'}], type:'sap.ui.model.type.Currency'}"}) 
+                        })
+                    ]
+                });
+                return customListItem;
+            }
+        }
     });
 }); 
